9a0d569f875ee8f06511943b2b9aa9fe06e538a0
linfaxin <673636904@qq.com>
2014-11-10 02:02:32 +0800

    add gen to git ignore

--- android-pdfview/gen/com/joanzapata/pdfview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.joanzapata.pdfview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- faw-vw/gen/com/fax/faw_vw/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.fax.faw_vw;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
